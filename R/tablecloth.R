fc07a72255d5db94445a1012d63331e044cf05b8
Haniah Kring <hrkring@haniahs-air.attlocal.net>
2023-07-22 13:01:14 -0400

    Added options for dynamic_table (Row names, grouping, table numbering, col renaming, col hiding)

--- R/tablecloth.R
+++ R/tablecloth.R
@@ -126,7 +126,7 @@ static_table <- function(data,
     gt_table <- gt_table |> cols_hide(all_of(hide_cols))
   }
 
-  # Row Renaming
+  # Column Renaming
   if (!identical(rename_cols, NULL)) {
     gt_table <- gt_table |> cols_label(.list = rename_cols)
   }
@@ -144,48 +144,91 @@ static_table <- function(data,
   return(gt_table)
 }
 
-#' Constructs the dynamic table with an optional theme using DT
+#' Construct a customized DT table
 #'
-#' @param data The table data
-#' @param title The title of the table
+#' @param data Input data.
+#' @param title The table title.
 #' @param theme The custom theme
 #'
 #' @return The formatted dynamic table
 #'
 #' @examples dynamic_table(financial_data, title = '2023 Income Statement', theme = 'greenscale')
-dynamic_table <- function(data, rowname_col, group_col, title, theme) {
+dynamic_table <- function(data,
+                          table_number = NULL,
+                          title = NULL,
+                          subtitle = NULL,
+                          rowname_col = NULL,
+                          group_col = NULL,
+                          aggregate_by = NULL,
+                          autoformat = TRUE,
+                          order_cols = NULL,
+                          rename_cols = NULL,
+                          hide_cols = NULL,
+                          is_color_coded = FALSE,
+                          theme = "default") {
 
-  # Row Names
-  if (!identical(rowname_col, NULL)) {
+  # Setting row names and grouping
+  if (!identical(rowname_col, NULL) & !identical(group_col, NULL)) {
     rownames <- as.character(data[[rowname_col]])
+    group <- list(dataSrc = which(names(data) == group_col))
+    order <- list(list(which(names(data) == rowname_col), 'asc'), list(which(names(data) == group_col), 'asc'))
+  }
+  else if (!identical(rowname_col, NULL) & identical(group_col, NULL)) {
+    rownames <- as.character(data[[rowname_col]])
+    order <- list(list(which(names(data) == rowname_col), 'asc'))
+  }
+  else if (identical(rowname_col, NULL) & !identical(group_col, NULL)) {
+    group <- list(dataSrc = which(names(data) == group_col))
+    order <- list(list(which(names(data) == group_col), 'asc'))
   } else {
     rownames <- ""
+    group <- NULL
+    order <- NULL
   }
+
+  # Table Numbering
+  if (!identical(table_number, NULL) & identical(title, NULL)) {
+    title <- paste0("Table ", table_number)
+  } else if (!identical(table_number, NULL) & !identical(title, NULL)) {
+    title <- paste0("Table ", table_number, ": ", title)
+  }
+
+  # Column Renaming
+  if (!identical(rename_cols, NULL)) {
+    colnames <- setNames(names(data), rename_cols)
+  } else {
+    colnames <- names(data)
+  }
+
+  # Column Hiding
+  cols_to_hide <- list(
+    visible=FALSE,
+    targets = which(names(data) %in% c(group_col, rowname_col, hide_cols))
+    )
+
+  # Color Coding
+
 
   # Create Table
   dt_table <- data |>
     datatable(
       rownames = rownames,
+      colnames = colnames,
       caption = htmltools::tags$caption(
-        style = "font-family: verdana; caption-side: top; text-align: center; font-weight: bold; font-size: 25px; font-style: normal;",
+        style = "font-family: verdana; caption-side: top; text-align: left; font-weight: bold; font-size: 25px; font-style: normal;",
         htmltools::em(title)
       ),
       extensions = c("Buttons", 'RowGroup'),
       options = list(
-        order = list(
-          list(which(names(data) == group_col), 'asc'),
-          list(which(names(data) == rowname_col), 'asc')
-        ),
-        rowGroup = list(dataSrc = which(names(data) == group_col)),
+        order = order,
+        rowGroup = group,
         dom = "Bfrtip",
         buttons = c("csv", "excel", "pdf"),
         deferRender = TRUE,
         scrollY = 350,
         columnDefs = list(
           list(className = "dt-center", targets = "_all"),
-          list(visible=FALSE,
-               targets=c(which(names(data) == group_col),
-                         which(names(data) == rowname_col)))
+          cols_to_hide
       )),
       class = "row-border hover compact stripe",
       fillContainer = TRUE
